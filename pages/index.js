8f6bd8e7f1225f18955f5799518a6fcd8a444fb2
Paul Friedman <paulrfri@gmail.com>
2020-06-14 13:28:07 -0700

    Use a web font
    *sigh*

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,10 @@ export default function Home() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌉</text></svg>"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main tw="mt-12 mb-auto mt-auto flex flex-row-reverse">
